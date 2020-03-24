b4e014b629c6fb87ea89843db436a26a04c85def
anddhong <andrewh1@andrew.cmu.edu>
2020-03-23 23:13:52 -0400

    updated the models

--- doMe/models.py
+++ doMe/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Workspace(models.Model):
-	members = models.ManyToManyField("self", related_name = "Users")
-	description = models.CharField(max_length=200)
-
 class Profile(models.Model):
 	user = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
 	profilePicture = models.FileField(blank=True)
+
+class Workspace(models.Model):
+	members = models.ManyToManyField(Profile, related_name = "Users")
+	description = models.CharField(max_length=200)
+	listItems = models.ManyToManyField(toDoItem)
 
 class toDoItem(models.Model):
 	class Priority(models.IntegerChoices):
@@ -22,4 +23,5 @@ class toDoItem(models.Model):
 	user = models.ForeignKey(User)
 	title = models.CharField(max_length=30)
 	description = models.CharField(max_length=150)
-	
+	dueDate = models.DateTimeField()
+
